0676e1ee4baa407d197d6bc71e776bf3e887ad24
Philip Boyde <philipboyde@icloud.com>
2024-03-15 20:30:39 +0100

    Static Webpage

--- TowerDefenseGame/src/js/Main.js
+++ TowerDefenseGame/src/js/Main.js
@@ -1,8 +1,27 @@
-const canvas = document.querySelector('canvas')
-const ctx  = canvas.getContext('2d')
+const gameCanvas = document.querySelector('.GameScreen');
+const interactiveCanvas = document.querySelector('.interactiveBar');
 
-canvas.width = 1280
-canvas.height = 1068
+if (gameCanvas) {
+    const gameCtx = gameCanvas.getContext('2d');
 
-ctx.fillStyle = 'white'
-ctx.fillRect(0,0 , canvas.width, canvas.height)
+    gameCanvas.width = 1280;
+    gameCanvas.height = 950;
+
+    gameCtx.fillStyle = 'white';
+    gameCtx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+} else {
+    console.error('Game canvas element not found!');
+}
+
+if (interactiveCanvas) {
+    const interactiveCtx = interactiveCanvas.getContext('2d');
+
+    interactiveCanvas.width = 358;
+    interactiveCanvas.height = 100;
+
+    interactiveCtx.fillStyle = '#A1662F';
+    interactiveCtx.fillRect(0, 0, interactiveCanvas.width, interactiveCanvas.height);
+} else {
+    console.error('Interactive canvas element not found!');
+}
+
