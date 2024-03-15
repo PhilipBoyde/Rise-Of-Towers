6e678a7078c6bf42356a1ee5e02d565c71af35da
Bojjan <116097397+Alkodog@users.noreply.github.com>
2024-03-15 19:39:00 +0100

    Website Main Menu

---
+++ TowerDefenseGame/src/js/Main.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas')
+const ctx  = canvas.getContext('2d')
+
+canvas.width = 1280
+canvas.height = 1068
+
+ctx.fillStyle = 'white'
+ctx.fillRect(0,0 , canvas.width, canvas.height)
