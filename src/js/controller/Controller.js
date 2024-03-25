c521b3692af1c79c2d3f44720323384603226e0b
Bojjan <116097397+Bojjjan@users.noreply.github.com>
2024-03-25 20:42:22 +0100

    PathFindingWorks

--- src/js/controller/Controller.js
+++ src/js/controller/Controller.js
@@ -1,15 +1,17 @@
 const gameCanvas = document.querySelector('.GameScreen');
 const interactiveCanvas = document.querySelector('.GameUI');
 const activeMap = 1;    // Change this to the map you want to load
-const gameCtx = gameCanvas.getContext('2d');
 import map1Controller from './map1Controller.js';
 
 
+/*
 window.addEventListener('resize', function()        {
     if (window.innerWidth < 1352  || window.innerHeight < 700) {
         alert('Your browser window is too small!');
     }
 });
+
+ */
 
 
 
@@ -35,31 +37,9 @@ window.addEventListener('click', function(event) {
 });
 
 
-// Draw the game map
-if (gameCanvas) {
-    
-    gameCanvas.width = 1124;
-    gameCanvas.height = 768;
-
-    let img = new Image();
-    img.onload = function() {
-        gameCtx.drawImage(img, 0, 0, gameCanvas.width, gameCanvas.height);
-
-        drawTowerHitBox(gameCtx);
-
-       
-        if (activeMap === 1) {
-            map1Controller(gameCtx, interactiveCanvas);
-
-            //import Map1TowerHitBox from '../model/map1/Map1TowerHitBox.js';
-            //Map1TowerHitBox();
-        }else {
-            console.error('Map not found!');
-        }
-    }
-    img.src = '../assets/gameMap/map1.png';
-} else {
-    console.error('Game canvas element not found!');
+if (gameCanvas){
+    gameCanvas.width = 1024;
+    gameCanvas.height = 640;
 }
 
 //draw the sidebar canvas
@@ -75,18 +55,13 @@ if (interactiveCanvas) {
     console.error('Interactive canvas element not found!');
 }
 
+if (activeMap === 1) {
+    map1Controller(gameCanvas, interactiveCanvas);
 
-
-
-function drawTowerHitBox(gameCtx){
-    gameCtx.strokeStyle = '#3cff00'; 
-    gameCtx.lineWidth = 2; 
-    
-    gameCtx.strokeRect(135, 70, 85, 80); //Tower 1
-    gameCtx.strokeRect(675, 70, 85, 80); //Tower 2
-    gameCtx.strokeRect(540, 265, 85, 80); //Tower 3
-    gameCtx.strokeRect(360, 265, 85, 80); //Tower 4
-    gameCtx.strokeRect(360, 685, 85, 80); //Tower 5
-    gameCtx.strokeRect(810, 535, 85, 80); //Tower 6
-    gameCtx.strokeRect(1035, 265, 85, 80); //Tower 7
+    //import Map1TowerHitBox from '../model/map1/Map1TowerHitBox.js';
+    //Map1TowerHitBox();
+}else {
+    console.error('Map not found!');
 }
+
+
