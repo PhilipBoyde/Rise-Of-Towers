a94b8ecb1bf6309aaeb1eaca528a1b9804d14250
Bojjan <116097397+Bojjjan@users.noreply.github.com>
2024-03-28 20:40:55 +0100

    Pathfinding and bug fixing
    + WaveButton
    + See all enemy bug fixed
    + code fixes

--- src/js/controller/Controller.js
+++ src/js/controller/Controller.js
@@ -1,67 +1,115 @@
+import {path1Route1} from "../model/map1/Map1Paths.js";
+import { Enemy } from './EnemyController.js';
+import {Startmap1 } from './map1Controller.js';
+
 const gameCanvas = document.querySelector('.GameScreen');
 const interactiveCanvas = document.querySelector('.GameUI');
-const activeMap = 1;    // Change this to the map you want to load
-import map1Controller from './map1Controller.js';
+const activeMapNbr = 1;    // Change this to the map you want to load
+let round = 0;
+let activeMap;
+document.getElementById("GameWaveButton").addEventListener("click", nexWave);
+const gameCtx = gameCanvas.getContext('2d');
 
-
-/*
-window.addEventListener('resize', function()        {
-    if (window.innerWidth < 1352  || window.innerHeight < 700) {
-        alert('Your browser window is too small!');
-    }
-});
-
- */
-
-
-
-
-
-window.addEventListener('click', function(event) {
-    const rect = gameCanvas.getBoundingClientRect();
-
-    
-    //TESTING
-    let rectX = 135;
-    let rectY = 70;
-    let rectWidth = 85;
-    let rectHeight = 80;
-
-    // Get the x and y coordinates of the click event
-    let pointX = event.clientX - rect.left;
-    let pointY = event.clientY - rect.top;
-
-    if (pointX >= rectX && pointX <= rectX + rectWidth && pointY >= rectY && pointY <= rectY + rectHeight) {
-        alert( 'Clicked within the specified area!')
-    }
-});
-
-
-if (gameCanvas){
-    gameCanvas.width = 1024;
-    gameCanvas.height = 640;
-}
-
-//draw the sidebar canvas
-if (interactiveCanvas) {
-    const interactiveCtx = interactiveCanvas.getContext('2d');
+if (gameCanvas && interactiveCanvas){
+    gameCanvas.width = 1120;
+    gameCanvas.height = 960;
 
     interactiveCanvas.width = 200;
     interactiveCanvas.height = 768;
 
+    const interactiveCtx = interactiveCanvas.getContext('2d');
     interactiveCtx.fillStyle = '#A1662F';
     interactiveCtx.fillRect(0, 0, interactiveCanvas.width, interactiveCanvas.height);
-} else {
-    console.error('Interactive canvas element not found!');
-}
-
-if (activeMap === 1) {
-    map1Controller(gameCanvas, interactiveCanvas);
-
-    //import Map1TowerHitBox from '../model/map1/Map1TowerHitBox.js';
-    //Map1TowerHitBox();
 }else {
-    console.error('Map not found!');
+    alert('Canvas not found!, Pleas try again later');
 }
+
+let img = new Image();
+changeMap()
+img.onload = () => {
+    gameCtx.drawImage(img, 0, 0, gameCanvas.width, gameCanvas.height);
+}
+
+function changeMap(){
+    switch (activeMapNbr) { // Load the map based on the activeMap variable
+        case 1:
+            img.src = '../assets/gamemap/Map1.png';
+            activeMap = new Startmap1();
+
+            break;
+        case 2:
+            img.src = '../assets/gamemap/Map2.png';
+
+            break;
+        case 3:
+            img.src = '../assets/gamemap/Map3.png';
+
+            break
+
+        default:
+            console.log('Map not found!')
+            break;
+    }
+}
+
+
+function nexWave(){
+    disableButton()
+    activeMap.nexWave(round);
+    
+    const enemies = test();
+    animate(enemies)
+}
+
+function disableButton(){
+    let button = document.getElementById("GameWaveButton");
+    button.style.backgroundColor = 'gray';
+    button.style.filter = 'blur(1px)';
+    document.getElementById("GameWaveButton").disabled = true;
+}
+
+function enableButton(){
+    let button = document.getElementById("GameWaveButton");
+    button.style.backgroundColor = '';
+    button.style.filter = 'none';
+    document.getElementById("GameWaveButton").disabled = false;
+    
+}
+
+
+
+
+function test() {
+    const enemies = [];
+    for (let i = 1; i <10;  i++){
+        const xOffSet = i * 80;
+        enemies.push(new Enemy({position: {x: path1Route1[0].x + xOffSet, y: path1Route1[0].y}}, 1, path1Route1, 100));
+    }
+
+    return enemies;
+}
+
+
+    function animate(enemies) {
+
+        if (enemies.length === 0) {
+            console.log('%cWave ' + round + ' Completed!', 'color: green; font-size: 20px;');
+            enableButton()
+            round++;
+            return;
+        }
+        
+        gameCtx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+        gameCtx.drawImage(img, 0, 0, gameCanvas.width, gameCanvas.height);
+        
+        console.log("Enemies", enemies);
+
+        enemies = enemies.filter(enemy => !enemy.update(gameCtx));
+
+
+
+
+        requestAnimationFrame(() => animate(enemies));
+    }
 
 
--- src/js/controller/map1Controller.js
+++ src/js/controller/map1Controller.js
@@ -1,98 +1,16 @@
 import { path1Route1, path1Route2, path1Route3 } from '../model/map1/Map1Paths.js';
-function map1Controller(gameCanvas, interactiveCanvas) {
-    const c = gameCanvas.getContext('2d');
-    startGame(c, gameCanvas)
-}
 
-function startGame(c, gameCanvas) {
-    if (c) {
+export class Startmap1{
+    constructor() {
+        console.log('Map1 Controller loaded');
+    }
+
+    nexWave(round){
         
-        let img = new Image();
-        img.onload = function() {
-            c.drawImage(img, 0, 0, gameCanvas.width, gameCanvas.height);
-            drawTowerHitBox(c,);
-            animate(c, gameCanvas, img)
-        }
-        
-        img.src = '../assets/gameMap/map1.png';
-    } else {
-        console.error('Game canvas element not found!');
-    }
-}
 
-
-function drawTowerHitBox(gameCtx){
-    gameCtx.strokeStyle = '#3cff00';
-    gameCtx.lineWidth = 2;
-
-    gameCtx.strokeRect(135, 70, 85, 80); //Tower 1
-    gameCtx.strokeRect(675, 70, 85, 80); //Tower 2
-    gameCtx.strokeRect(540, 265, 85, 80); //Tower 3
-    gameCtx.strokeRect(360, 265, 85, 80); //Tower 4
-    gameCtx.strokeRect(360, 685, 85, 80); //Tower 5
-    gameCtx.strokeRect(810, 535, 85, 80); //Tower 6
-    gameCtx.strokeRect(1035, 265, 85, 80); //Tower 7
-}
-
-
-class Enemy {
-    constructor({position = {x: 0, y: 0}}, speed, path){
-        this.position = position;
-        this.width = 50;
-        this.height = 50;
-        this.speed = speed;
-        this.path = path;
-        this.pathIndex = 0;
-
-        this.center = {
-            x: this.position.x + this.width / 2,
-            y: this.position.y + this.height / 2
-
-        }
-    }
-    draw(gameCtx) {
-        gameCtx.fillStyle = '#ff0000';
-        gameCtx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
-    update(gameCtx) {
-        this.draw(gameCtx);
-
-        const path = this.path[this.pathIndex]
-        console.log(this.position.y)
-        const yDistance = path.y -  this.center.y
-        const xDistance = path.x -  this.center.x
-        
-        const angle = Math.atan2(yDistance, xDistance);
-        this.position.x += Math.cos(angle) * this.speed;
-        this.position.y += Math.sin(angle) * this.speed;
-        this.center = {
-            x: this.position.x + this.width / 2,
-            y: this.position.y + this.height / 2
-        }
-
-        if (Math.round(this.center.x) === Math.round(path.x) &&  Math.round(this.center.y) === Math.round(path.y) && this.pathIndex < this.path.length - 1) {
-            this.pathIndex++;
-        }
-    }
-}
-const enemy = new Enemy({position: {x: path1Route1[0].x, y: path1Route1[0].y}}, 0.2, path1Route1);
-
-function animate(gameCtx, gameCanvas, img){
-    requestAnimationFrame(() => animate(gameCtx, gameCanvas, img));
-    gameCtx.clearRect(0,0,gameCanvas.width, gameCanvas.height);
-    gameCtx.drawImage(img, 0, 0, gameCanvas.width, gameCanvas.height);
-
-    enemies.forEach(enemy => {
-        enemy.update(gameCtx);
-    });
 }
 
 
-const enemies = [];
-for (let i = 1; i <10;  i++){
-    const xOffset = i * 60;
-    enemies.push(new Enemy({position: {x: path1Route1[0].x - xOffset, y: path1Route1[0].y}}, 0.2, path1Route1));
-}
 
-export default map1Controller;
