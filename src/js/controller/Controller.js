714d54d4ef3ed1e042419e643c09e19625285fe3
Bojjan <116097397+Bojjjan@users.noreply.github.com>
2024-04-04 21:52:01 +0200

    Average FPS Counter
    Added an average FPS Counter for animation frames

--- src/js/controller/Controller.js
+++ src/js/controller/Controller.js
@@ -120,28 +120,58 @@ function reduceHealth(){
  * @param enemies
  * @author Philip,
  */
+let lastTime = 0;
+const fpsInterval = 1000 / 60; // Assuming 60 FPS
+let lastFpsUpdateTime = 0;
+let currentTime;
+let elapsed;
+
 function animate(enemies) {
-    
+    currentTime = performance.now();
+    elapsed = currentTime - lastTime;
+
+    if (elapsed > fpsInterval) { //Limit the frame rate to about 60 FPS
+        lastTime = currentTime - (elapsed % fpsInterval);
+
+        // Check if all enemies are dead
         if (enemies.length === 0) {
             console.log('%cWave ' + round + ' Completed!', 'color: green; font-size: 20px;');
-            enableButton()
+            enableButton();
             round++;
             return;
         }
-        const animationID = requestAnimationFrame(() => animate(enemies));
-        
+
         gameCtx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-        gameCtx.drawImage(img, 0, 0, gameCanvas.width, gameCanvas.height);
-        
-        //console.log("Enemies", enemies);
+        gameCtx.drawImage(img, 0, 0, gameCanvas.width, gameCanvas.height); // !!Try to optimize this, unnecessary to redraw each frame!!
 
         enemies = enemies.filter(enemy => !enemy.update(gameCtx, reduceHealth));
 
-        if (playerHealth <= 0){
+        // Check if player health is 0
+        if (playerHealth <= 0) {
             document.querySelector('.GameOver').style.display = 'flex';
             console.log('%cGAME OVER!', 'color: red; font-size: 20px;');
-            cancelAnimationFrame(animationID)
+            cancelAnimationFrame(animationID);
         }
+
+
     }
 
+    // Update FPS counter
+    fpsCounterUpdate(1000 / elapsed);
 
+    // Request next frame
+    const animationID = requestAnimationFrame(() => animate(enemies));
+}
+
+let frameCount = 0;
+let fpsAccumulator = 0;
+function fpsCounterUpdate(fps){
+    frameCount++;
+    fpsAccumulator += fps;
+
+    if (frameCount % 60 === 0) { // Update every 60 frames
+        const averageFPS = fpsAccumulator / 60;
+        document.querySelector('.fpsCounter').innerHTML = 'Average<br>FPS: ' + averageFPS.toFixed(2);
+        fpsAccumulator = 0;
+    }
+}
